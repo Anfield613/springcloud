4b9fa1b744a8ce313090b69f29ac2961d7bdbc47
Fill <MrFill0613@outlook.com>
2020-10-27 17:27:21 +0800

    添加nacos的服务发现

---
+++ nacos-search-provider/src/main/java/com/fill/springcloud/ProviderNacosMain.java
@@ -0,0 +1,16 @@
+package com.fill.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProviderNacosMain {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderNacosMain.class, args);
+    }
+
+}
