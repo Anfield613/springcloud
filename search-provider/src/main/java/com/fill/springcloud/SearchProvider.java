57fea375d921098df77487adcb11fe26460409da
fangzizhong <fangzz@smartdot.com>
2020-10-27 22:42:38 +0800

    整合swagger3配置

--- search-provider/src/main/java/com/fill/springcloud/SearchProvider.java
+++ search-provider/src/main/java/com/fill/springcloud/SearchProvider.java
@@ -4,13 +4,15 @@ package com.fill.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class NacosSearchProvider {
+@EnableOpenApi
+public class SearchProvider {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosSearchProvider.class, args);
+        SpringApplication.run(SearchProvider.class, args);
     }
 
 }
